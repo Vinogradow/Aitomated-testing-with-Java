ee5f958b9657b8d0e66d7278b2009a5be78a5d1b
Vinogradow <panarin92@gmail.com>
2022-11-10 13:45:39 +0200

    Automated testing with Java

---
+++ src/epam/com/learn/main/InputStringProgram.java
@@ -0,0 +1,13 @@
+package epam.com.learn.main;
+
+import java.util.Scanner;
+
+public class InputStringProgram
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        System.out.println("hello " + input);
+    }
+}
